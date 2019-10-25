b1c96dad2cd1dde26f658755d0e3c575edc882a7
Dan Stoner <dan.stoner@sharpspring.com>
2019-10-25 08:06:03 -0400

    add command line argument parsing

--- ats.py
+++ ats.py
@@ -2,15 +2,9 @@
    Suitable for copying and pasting into Slack"""
 
 from art import *
+import argparse
 
-font = "5x7" # Uses only hash chars in output
-
-# To generalize, do some argparsing instead of these
-emoji = "airhorn1"
-blank_emoji = "blank"  # would work better with an actually fully blank emoji
-message = "HONK"
-
-def emojify(ascii_art):
+def emojify(ascii_art, emoji, blank_emoji):
     emojied = []
     emojied.append(colonify(blank_emoji))
     for each in ascii_art:
@@ -31,8 +25,19 @@ def colonify(emoji_text):
     return "".join([":", emoji_text, ":"])
 
 def main():
-    Art = text2art(message, font)
-    Emojified = emojify(Art)
+    parser = argparse.ArgumentParser(description='text to ascii art to slack')
+    # '5x7' is a tall skinny font that only uses hashtags in output
+    parser.add_argument("--font", default='5x7', help='name of art lib font to use')
+    parser.add_argument("-e", "--emoji", default='smile', help='emoji name')
+    # a completely blank emoji would work best, but this is an ok substitute in the efault emoji pack
+    parser.add_argument("-b", "--blank-emoji", default='white_small_square')
+    parser.add_argument("-m", "--message", required=True, help='short text message')
+    args=parser.parse_args()
+    print()
+    print(f"Converting '{args.message}' to representation using {colonify(args.emoji)}")
+
+    Art = text2art(args.message, args.font)
+    Emojified = emojify(Art, args.emoji, args.blank_emoji)
     print()
     print(Art)
     print()
